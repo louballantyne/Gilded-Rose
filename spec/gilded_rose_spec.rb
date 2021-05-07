cd5d865e04c97bef8201970c54699c0bd368c391
Louise Ballantyne <lousballantyne@icloud.com>
2021-05-07 09:30:43 +0100

    Refactored tests to improve maintainability

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -77,13 +77,13 @@ describe GildedRose do
     it "does not increase the quality of Aged Brie beyond 50" do
       brie = [Item.new("Aged Brie", 10, 50)]
       shop = GildedRose.new(brie)
-      35.times { shop.update_quality }
-      expect( brie[0].quality ).to eq 50
+      shop.update_quality
       expect{ shop.update_quality }.to change { brie[0].quality }.by 0
+      expect( brie[0].quality ).to eq 50
     end
 
     it "does not decrease quality below 0" do
-      vest = [Item.new(name = "+5 Dexterity Vest", sell_in = 10, quality = 10)]
+      vest = [Item.new("+5 Dexterity Vest", 10, 10)]
       shop = GildedRose.new(vest)
       20.times { shop.update_quality }
       expect(vest[0].quality).to eq(0)
@@ -91,16 +91,13 @@ describe GildedRose do
   end
 
   context 'with time, conjured items degrade in quality twice as quickly as other items' do
+    cake = [Item.new(name = "Conjured Mana Cake", 1, 10)]
+    shop = GildedRose.new(cake)
     it 'decreases in quality by 2 in a day, up until the sell-by date' do
-      cake = [Item.new(name = "Conjured Mana Cake", sell_in = 10, quality = 10)]
-      shop = GildedRose.new(cake)
       expect{ shop.update_quality }.to change { cake[0].quality }.by(-2)
     end
 
     it 'decreases in quality by 4 in a day, after the sell-by date' do
-      cake = [Item.new(name = "Conjured Mana Cake", sell_in = 2, quality = 10)]
-      shop = GildedRose.new(cake)
-      2.times { shop.update_quality }
       expect{ shop.update_quality }.to change { cake[0].quality }.by(-4)
     end
   end
